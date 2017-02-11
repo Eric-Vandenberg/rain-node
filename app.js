f5c1f3e900f80a6522b41b07a34d441c9a3884a4
Eric Vandenberg <Eric1827v@gmail.com>
2017-02-10 19:04:29 -0800

    Dynamic encoded address lookup

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
+`use strict`;
+
 const request = require(`request`);
+const yargs = require(`yargs`);
+
+const argv = yargs
+    .options({
+        address: {
+            describe: `Address for lookup`,
+            demand: true,
+            alias: `a`,
+            string: true
+        }
+    })
+    .help()
+    .alias(`help`, `h`)
+    .argv;
+
+let encodedURI = encodeURIComponent(argv.a);
 
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedURI}`,
     json: true
 }, (err, res, body) => {
     if (err) throw err;
-    console.log(JSON.stringify(body, undefined, 4));
+    
+    // console.log(JSON.stringify(res, undefined, 4));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat} \nLng: ${body.results[0].geometry.location.lng}`);
 });
