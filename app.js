d7f270da9a23395dd135acc42b9cc07113742850
Eric Vandenberg <Eric1827v@gmail.com>
2017-02-10 19:37:30 -0800

    Callback err handling

--- app.js
+++ app.js
@@ -22,9 +22,14 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedURI}`,
     json: true
 }, (err, res, body) => {
-    if (err) throw err;
-    
-    // console.log(JSON.stringify(res, undefined, 4));
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Lat: ${body.results[0].geometry.location.lat} \nLng: ${body.results[0].geometry.location.lng}`);
+    if (err || res.statusCode !== 200) {
+        console.log(`Unable to connect to Google's servers`);
+    }
+    else if (body.status === 'ZERO_RESULTS') {
+        console.log(`Unable to retrieve info for that address`);
+    }
+    else if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Lat: ${body.results[0].geometry.location.lat} \nLng: ${body.results[0].geometry.location.lng}`);
+    }
 });
